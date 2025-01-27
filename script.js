920ec27d114508d3e2fbc0c70eedede309266b7c
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2025-01-27 23:30:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+   let ans=[];
+	let currarr=[]
+	let currsum=0;
+	for(let num of arr){
+		if(currsum +num > n){
+			ans.push(currarr);
+			currsum=0; 
+			currarr=[]
+		}
+		currarr.push(num);
+		currsum+=num;
+	}
+	if(currarr.length>0){
+		ans.push(currarr);
+	}
+	return ans;
 };
 
 const n = prompt("Enter n: ");
